3d6f074ca909d6f53124894694d4ee5758d8d73b
sivanuz23-meet <109904738+sivanuz23-meet@users.noreply.github.com>
2023-03-09 12:12:36 +0200

    Finished Labs

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements View.OnCapturedPo
                             startActivity(i);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(MainActivity.this, "Authentication failed",
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
